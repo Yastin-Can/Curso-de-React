aad2307fa0478f893e422bfbf5332c8960b25e5e
Yastin-Can <yastinvillarroel2005@gmail.com>
2024-11-09 20:58:45 -0300

    cambie por props :o

--- Proyectos/proyects/mi-app/src/userCard.jsx
+++ Proyectos/proyects/mi-app/src/userCard.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 
-export function UserCard({ children, img, prof, ubi, descript }) {
+export function UserCard(props) {
     return (
         <div className='section-container'>
-            <img src={`https://www.freecodecamp.org/static/${img}`} alt="" className='section-img'/>
+            <img src={`https://www.freecodecamp.org/static/${props.img}`} alt="" className='section-img'/>
             <div className='section-container-info'>
-                <p className="section-p name">{children} in {ubi}</p>
-                <p className="section-p profec">{prof}</p>
-                <p className="section-p-descrip">{descript}</p>
+                <p className="section-p name">{props.children} in {props.ubi}</p>
+                <p className="section-p profec">{props.prof}</p>
+                <p className="section-p-descrip">{props.descript}</p>
             </div>
         </div>
     );
