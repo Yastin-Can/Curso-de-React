1b41a6913e51ffda7cbf4f00648ad96ce206dfbb
Yastin-Can <yastinvillarroel2005@gmail.com>
2024-11-10 00:10:43 -0300

    mejor optimizado

---
+++ Proyectos/proyects/mi-app/src/users.jsx
@@ -0,0 +1,23 @@
+export const users = [
+    {
+      userName: 'Shawn Wang in ',
+      img: 'Shawn-75f7dad2bfcbae1988440b303b5a465b.png',
+      prof: 'Software Engineer at Amazon',
+      ubi: 'Singapore',
+      descript: '"It´s scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."'
+    },
+    {
+      userName: 'Sarah Chima in ',
+      img: 'Sarah-04dee53ff6709a3b1c0bbe0023554a46.png',
+      prof: 'Software Engineer at ChatDesk',
+      ubi: 'Nigeria',
+      descript: '"freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."'
+    },
+    {
+      userName: 'Emma Bostian in ',
+      img: 'Emma-1384890b0c332b769f67e8c14b628e13.png',
+      prof: 'Software Engineer at Spotify',
+      ubi: 'Sweden',
+      descript: '"I´ve always struggled with learning JavaScript. I´ve taken many courses but freeCodeCamp´s course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."'
+    },
+  ]
